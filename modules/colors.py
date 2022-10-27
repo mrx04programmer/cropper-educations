851ab69fc091931ab11fd2db3f24c04e2327e95f
mrx04programmer <mjuandavid901@gmail.com>
2022-10-26 22:43:17 -0500

    Modulos de cropper necesarios para su funcionamiento

---
+++ modules/colors.py
@@ -0,0 +1,10 @@
+## Colors
+# Author: Mrx04programmer
+# Github : https://github.com/mrx04programmer
+W = '\033[37m' # Default
+R = '\033[1;31m'  # red
+G = '\033[1;32m'  # green
+O = '\033[0;33m'  # orange
+B = '\033[1;34m'  # blue
+P = '\033[1;35m'  # purple
+C = '\033[1;36m'  # cyan
